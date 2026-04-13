df5518d38082eab125a4984f88a3614ec3c72cd9
nicolenyeve <nicolenyeve5@gmail.com>
2026-04-13 20:49:48 +0200

    Update Main.java

--- POE_PART_ONES/src/Main.java
+++ POE_PART_ONES/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+//Register user
         REGISTRATION registration = new REGISTRATION();
         registration.Register();
 
