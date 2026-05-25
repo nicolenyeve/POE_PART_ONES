58d6b76dea21afb9cc44e94cd4e4e461f5c49271
nicolenyeve <nicolenyeve5@gmail.com>
2026-05-25 12:36:12 +0200

    Update LOGIN.java

--- POE_PART_ONES/src/LOGIN.java
+++ POE_PART_ONES/src/LOGIN.java
@@ -1,45 +1,49 @@
 import java.util.Scanner;
 
 public class LOGIN {
-    private String UserName;
+    private String userName;
     private String password;
     private String firstName;
     private String lastName;
+    private static final int MAX_ATTEMPTS = 3;
 
     // Constructor
-    public LOGIN(String UserName, String password, String firstName, String lastName) {
-        this.UserName = UserName;
+    public LOGIN(String userName, String password, String firstName, String lastName) {
+        this.userName = userName;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    // Method to attempt login
+    // Method to attempt login with retry limit
     public String loginUser(String enteredUserName, String enteredPassword) {
+        Scanner scanner = new Scanner(System.in);
+        int attempts = 1;
 
-
-        if (enteredUserName.equals(UserName) && enteredPassword.equals(password)) {
+        // Check first attempt
+        if (enteredUserName.equals(userName) && enteredPassword.equals(password)) {
             System.out.println("Welcome back " + this.firstName + " " + this.lastName + "! It's great to see you again.");
+            MessageApp.startChat();
             return "Login successful for: " + firstName;
         }
 
-
-        Scanner scanner = new Scanner(System.in);
-        boolean loginSuccess = false;
-
-        while (!loginSuccess) {
-            System.out.println("Incorrect username or password. Please try again.");
+        // Retry up to MAX_ATTEMPTS - 1 more times
+        while (attempts < MAX_ATTEMPTS) {
+            System.out.println("Incorrect username or password. Attempt " + attempts + " of " + MAX_ATTEMPTS);
             System.out.print("Please re-enter your username: ");
             enteredUserName = scanner.nextLine();
             System.out.print("Please re-enter your password: ");
             enteredPassword = scanner.nextLine();
 
-            if (enteredUserName.equals(UserName) && enteredPassword.equals(password)) {
+            if (enteredUserName.equals(userName) && enteredPassword.equals(password)) {
                 System.out.println("Welcome back " + this.firstName + " " + this.lastName + "! It's great to see you again.");
-                loginSuccess = true;
+
+                return "Login successful for: " + firstName;
             }
+            attempts++;
         }
 
-        return "Login successful for: " + firstName;
+        System.out.println("Too many failed attempts. Access denied.");
+        return "Login failed - maximum attempts exceeded";
     }
 }
